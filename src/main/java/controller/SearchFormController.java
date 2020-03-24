1ff848ed0c2bdf472d1ce80151aaacee33d35663
PetrIlya <airkills@yandex.by>
2020-03-24 16:45:17 +0300

    Confirmed version.

--- src/main/java/controller/SearchFormController.java
+++ src/main/java/controller/SearchFormController.java
@@ -39,6 +39,6 @@ public class SearchFormController {
                 filter(this.form::meetsSearchRequirements).
                 forEach(this.foundedRecords::add);
         this.table.hardUpdate();
-        new Alert(Alert.AlertType.INFORMATION, "Founded: " + this.foundedRecords.size()).show();
+        new Alert(Alert.AlertType.NONE, "Founded: " + this.foundedRecords.size()).show();
     }
 }
