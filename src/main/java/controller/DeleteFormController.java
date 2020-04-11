aaef2abf57677c8c6152990320de316e938a2f95
PetrIlya <airkills@yandex.by>
2020-04-11 11:55:10 +0300

    Unclosable alert bug fixed.

--- src/main/java/controller/DeleteFormController.java
+++ src/main/java/controller/DeleteFormController.java
@@ -45,6 +45,6 @@ public class DeleteFormController {
         this.table.hardUpdate();
         this.mainTable.hardUpdate();
 
-        new Alert(Alert.AlertType.NONE, "Deleted: " + this.deletedRecords.size()).show();
+        new Alert(Alert.AlertType.INFORMATION, "Deleted: " + this.deletedRecords.size()).show();
     }
 }
